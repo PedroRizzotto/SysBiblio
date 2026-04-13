ec45b0a3f06633fdff28e902d681a8b706e1e59a
Pedro H. Rizzotto <pedrohenriquerizzotto1@gmail.com>
2026-04-12 23:11:35 -0300

    commit inicial

---
+++ LivroService.java
@@ -0,0 +1,75 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LivroService {
+    private List<Livro> acervo = new ArrayList<>();
+
+    public Livro validaLivro(Livro novoLivro) throws Exception {
+        if (novoLivro == null) {
+            throw new Exception("Objeto Nulo");
+        }
+        if (novoLivro.getTitulo() == null || novoLivro.getTitulo().isEmpty())
+            throw new Exception("Título inválido");
+
+        novoLivro.setTitulo(novoLivro.getTitulo().trim().toUpperCase());
+
+        if (novoLivro.getAutor() == null || novoLivro.getTitulo().isEmpty())
+            throw new Exception("Autor inválido");
+
+        novoLivro.setAutor(novoLivro.getAutor().trim().toUpperCase());
+
+        if (novoLivro.getAnoPublicacao() < 1900
+                || novoLivro.getAnoPublicacao() > LocalDate.now().getYear())
+            throw new Exception("Ano de publicação inválido");
+
+        return novoLivro;
+    }
+
+    public void cadastrar(Livro novoLivro) throws Exception {
+
+        Livro novoLivroValidado = validaLivro(novoLivro);
+
+        for (Livro livro : acervo) {
+            if (livro.getTitulo().equalsIgnoreCase(novoLivroValidado.getTitulo())
+                    && livro.getAutor().equalsIgnoreCase(novoLivroValidado.getAutor())
+                    && livro.getAnoPublicacao() == novoLivroValidado.getAnoPublicacao())
+                throw new Exception("Já existe livro cadastrado com este Titulo, Autor, e Ano de publicação");
+        }
+
+        // Nesta parte estaria chamando a camada Repository
+        acervo.add(novoLivroValidado);
+    }
+
+    public List<Livro> listar() {
+        return acervo;
+    }
+
+    public List<Livro> pesquisar(String titulo) {
+        List<Livro> livrosEncontrados = new ArrayList<>();
+        titulo = titulo.toUpperCase();
+        for (Livro livro : acervo) {
+            if (livro.getTitulo().contains(titulo))
+                livrosEncontrados.add(livro);
+        }
+        return livrosEncontrados;
+    }
+
+    public void remover(int indice) throws Exception {
+        if (indice <= 0 || indice > acervo.size())
+            throw new Exception("Indíce inválido");
+        acervo.remove(indice);
+    }
+
+    public Livro retornarLivro(int indice) throws Exception {
+        if (indice < 0 || indice > acervo.size())
+            throw new Exception("Indíce inválido");
+        return acervo.get(indice);
+    }
+
+    public void editar(int indice, Livro livro) throws Exception {
+        Livro livroValidado = validaLivro(livro);
+
+        acervo.set(indice, livroValidado);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,115 @@
+import java.util.List;
+
+// dependências
+
+LivroService service = new LivroService();
+
+void main() {
+    String menu = """
+            ===== SysBiblio =====
+            1 - Cadastrar Livro
+            2 - Listar Livros
+            3 - Pesquisar Livros
+            4 - Remover Livro
+            5 - Editar Livro
+            0 - Sair
+            """;
+
+    int opcao;
+    do {
+        IO.print(menu);
+        opcao = Input.scanInt("Digite uma opção: ");
+        try {
+            switch (opcao) {
+                case 1 -> cadastrar();
+                case 2 -> listar();
+                case 3 -> pesquisar();
+                case 4 -> remover();
+                case 5 -> editar();
+                case 0 -> IO.println("Até breve!!!");
+                default -> IO.println("Opção Inválida");
+            }
+        } catch (Exception e) {
+            IO.println("ERRO: " + e.getMessage());
+        }
+        IO.readln("Pressione ENTER para continuar...");
+    } while (opcao != 0);
+}
+
+void cadastrar() throws Exception {
+    String titulo = Input.scanString("Digite o título do livro: ");
+    String autor = Input.scanString("Digite o autor do livro: ");
+    int anoPublicacao = Input.scanInt("Digite o ano de publicação do livro: ");
+    int numeroPaginas = Input.scanInt("Digite o número de páginas do livro: ");
+
+    Livro novoLivro = new Livro(titulo, autor, anoPublicacao, numeroPaginas);
+
+    service.cadastrar(novoLivro);
+
+    IO.println("Livro cadastrado com sucesso!!!");
+}
+
+void listar() {
+
+    List<Livro> livros = service.listar();
+    imprimirLista(livros);
+
+}
+
+void pesquisar() {
+
+    String pesquisa = Input.scanString("Digite parte do título: ");
+
+    List<Livro> livros = service.pesquisar(pesquisa);
+    imprimirLista(livros);
+
+}
+
+void imprimirLista(List<Livro> livros) {
+    if (livros.isEmpty()) {
+        IO.println("Nenhum livro encontrado!");
+        return;
+    }
+    int i = 1;
+    for (Livro livro : livros) {
+        IO.println(i++ + " - " + livro);
+    }
+
+}
+
+void remover() throws Exception {
+    listar();
+    int indice = Input.scanInt("Digite o número do livro que você deseja remover: ");
+    service.remover(indice - 1);
+    IO.println("Livro removido com sucesso");
+
+}
+
+void editar() throws Exception {
+    listar();
+    int indice = Input
+            .scanInt("Digite o número do livro que você deseja editar: ");
+    Livro livro = service.retornarLivro(indice - 1);
+
+    IO.println("Editando o livro " + livro.getTitulo() + "(pressione ENTER para não modificar)");
+
+    String novoTitulo = Input.scanString("Título (" + livro.getTitulo() + "): ", true);
+    if (novoTitulo == "")
+        novoTitulo = livro.getTitulo();
+
+    String novoAutor = Input.scanString("Autor (" + livro.getAutor() + "): ", true);
+    if (novoAutor == "")
+        novoAutor = livro.getAutor();
+
+    int novoAno = Input.scanInt("Ano (" + livro.getAnoPublicacao() + "): ", true);
+    if (novoAno == 0)
+        novoAno = livro.getAnoPublicacao();
+
+    int novoNuPaginas = Input.scanInt("N. Páginas (" + livro.getNumeroPaginas() + "): ", true);
+    if (novoNuPaginas == 0)
+        novoNuPaginas = livro.getNumeroPaginas();
+
+    Livro livroEditado = new Livro(novoTitulo, novoAutor, novoAno, novoNuPaginas);
+
+    service.editar(indice - 1, livroEditado);
+}
